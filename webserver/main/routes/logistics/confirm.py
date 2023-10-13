a7bf43c63d8ee34026a660e8611e1470f67d4e35
Gajendra Mehra <gajendramehra@WILASSETA0182.local>
2023-10-13 10:11:49 +0530

    release: v1.1.0

--- webserver/main/routes/logistics/confirm.py
+++ webserver/main/routes/logistics/confirm.py
@@ -17,42 +17,50 @@ confirm_namespace = Namespace('confirm', description='Confirm Namespace')
 
 @confirm_namespace.route("/v1/confirm")
 class ConfirmOrder(Resource):
-    path_schema = get_json_schema_for_given_path('/confirm')
-
-    @expects_json(path_schema)
     def post(self):
-        response_schema = get_json_schema_for_response('/confirm')
-        resp = get_ack_response(ack=True)
         payload = request.get_json()
-        log(json.dumps({f'{request.method} {request.path} req_body': json.dumps(payload)}))
-        dump_request_payload(payload, domain=OndcDomain.LOGISTICS.value)
-        message = {
-            "request_type": "logistics_confirm",
-            "message_ids": {
-                "confirm": payload[constant.CONTEXT]["message_id"]
+        print(f'------------------- {payload[constant.CONTEXT]["core_version"]}')
+        path_schema = get_json_schema_for_given_path('/confirm', payload[constant.CONTEXT]["core_version"])
+
+        @expects_json(path_schema)
+        def innerFunction():
+            response_schema = get_json_schema_for_response('/confirm', payload[constant.CONTEXT]["core_version"])
+            resp = get_ack_response(ack=True)
+            log(json.dumps({f'{request.method} {request.path} req_body': json.dumps(payload)}))
+            dump_request_payload(payload, domain=OndcDomain.LOGISTICS.value)
+            message = {
+                "request_type": "logistics_confirm",
+                "message_ids": {
+                    "confirm": payload[constant.CONTEXT]["message_id"]
+                }
             }
-        }
-        send_message_to_queue_for_given_request(message)
-        validate(resp, response_schema)
-        return resp
+            send_message_to_queue_for_given_request(message)
+            validate(resp, response_schema)
+            return resp
+    
+        return innerFunction()
 
 
 @confirm_namespace.route("/v1/on_confirm")
 class OnConfirmOrder(Resource):
-    path_schema = get_json_schema_for_given_path('/on_confirm')
-
-    @expects_json(path_schema)
     def post(self):
-        response_schema = get_json_schema_for_response('/on_confirm')
-        resp = get_ack_response(ack=True)
         payload = request.get_json()
-        dump_request_payload(payload, domain=OndcDomain.LOGISTICS.value)
-        message = {
-            "request_type": f"{OndcDomain.LOGISTICS.value}_on_confirm",
-            "message_ids": {
-                "on_confirm": payload[constant.CONTEXT]["message_id"]
+        # print(f'------------------- {payload[constant.CONTEXT]["core_version"]}')
+        path_schema = get_json_schema_for_given_path('/on_confirm', payload[constant.CONTEXT]["core_version"])
+
+        @expects_json(path_schema)
+        def innerFunction():
+            response_schema = get_json_schema_for_response('/on_confirm', payload[constant.CONTEXT]["core_version"])
+            resp = get_ack_response(ack=True)
+            dump_request_payload(payload, domain=OndcDomain.LOGISTICS.value)
+            message = {
+                "request_type": f"{OndcDomain.LOGISTICS.value}_on_confirm",
+                "message_ids": {
+                    "on_confirm": payload[constant.CONTEXT]["message_id"]
+                }
             }
-        }
-        send_message_to_queue_for_given_request(message)
-        validate(resp, response_schema)
-        return resp
+            send_message_to_queue_for_given_request(message)
+            validate(resp, response_schema)
+            return resp
+    
+        return innerFunction()
